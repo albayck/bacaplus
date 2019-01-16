a118d6e0f9cfe1c6a0346da5f6e722a1d7e4f5e2
albayck <35208924+albayck@users.noreply.github.com>
2019-01-17 02:54:38 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$uid="Facebook_952096518315697";
+$raw_uid="e6e2beee-5159-4053-86b8-413b6887d423";
+$user_name="Yusdi+Jordhi+Albayck";
+$invite_code="F4MMByMimn";
+$own_mac="02%3A00%3A00%3A00%3A00%3A00";
+$android_id="66d3a80b14b50f96";
+$device_type="Xiaomi++Redmi+4X";
+
+
+?>
